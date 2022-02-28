6f836d80df216cf13e2e4547d1952e66d5f63cf7
BradlyKim <kjh5864@naver.com>
2022-02-28 15:27:53 +0900

    연도를 기반으로 > 나이 계산하는 함수 추가  - 학생 데이터의 기능으로 작성

--- app/src/main/java/com/neppplus/listview_20220226/datas/StudentData.kt
+++ app/src/main/java/com/neppplus/listview_20220226/datas/StudentData.kt
@@ -1,8 +1,21 @@
 package com.neppplus.listview_20220226.datas
 
+import java.time.Year
+
 class StudentData(
     val name: String,
     val birthYear: Int,
     val address: String,
 ) {
+
+//    연도를 받아서 ? 그 해의 나이를 계산해주는 함수
+
+    fun getKoreanAge( year: Int ) : Int {
+
+        val age = year - birthYear + 1
+
+        return age
+
+    }
+
 }
